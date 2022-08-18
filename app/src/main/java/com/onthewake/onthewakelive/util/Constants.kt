91483f5f57ef00f9985e17b972166b6ace82eb2f
JustAmalll <amalkrasavchik@gmail.com>
2022-08-18 18:19:57 +0600

    URL changed

--- app/src/main/java/com/onthewake/onthewakelive/util/Constants.kt
+++ app/src/main/java/com/onthewake/onthewakelive/util/Constants.kt
@@ -1,15 +1,12 @@
 package com.onthewake.onthewakelive.util
 
 object Constants {
-    const val BASE_URL = "http://192.168.0.77:8080"
-    const val WS_BASE_URL = "ws://192.168.0.77:8080"
+    const val BASE_URL = "https://on-the-wake-live.herokuapp.com"
+    const val WS_BASE_URL = "ws://on-the-wake-live.herokuapp.com"
 
     const val PREFS_FIRST_NAME = "firstName"
     const val PREFS_USER_ID = "userID"
     const val PREFS_JWT_TOKEN = "jwt_token"
-
-    const val NOTIFICATION_CHANNEL_ID = "Queue Notification Channel ID"
-    const val NOTIFICATION_CHANNEL_NAME = "Queue Notification Channel"
 
     const val FIRST_ADMIN_USER_ID = "62f7af2f33e0d47030d73849"
     const val SECOND_ADMIN_USER_ID = "62fe27a88f997574d627b848"
